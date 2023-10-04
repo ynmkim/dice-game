d080844a30c42f2dd4dfdb7397df855a7193f8f0
김윤미 <younmeet@gmail.com>
2023-10-04 15:17:02 +0900

    Children Prop 활용하기 전

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import Button from "./Button";
 import Dice from "./Dice";
 
 function App() { 
   return (
     <div>
-      <Dice />
+      <div>
+        <Button text="던지기" />
+        <Button text="처음부터" />
+      </div>
       <Dice color="red" num={2} />
     </div>
   )
